202f02754a5e339079377828bd623bb4318bb7b8
Andy Armstrong <andy@hexten.net>
2007-08-19 11:06:53 +0000

    Much smaller.

--- benchmark/synthtest.pl
+++ benchmark/synthtest.pl
@@ -6,5 +6,5 @@ use warnings;
 my $count = shift || 1000;
 
 print qq{print "1..$count\\n";\n};
-print qq{print "ok $_ some test or other\\n";\n} for ( 1 .. $count );
+print qq{print "ok \$_ some test or other\\n" for ( 1 .. $count );\n};
 
