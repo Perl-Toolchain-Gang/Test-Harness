2369bc24eedd73b032f482415d38a3c36c4e64ce
Andy Armstrong <andy@hexten.net>
2007-09-05 18:28:32 +0000

    Added an example of using callbacks to hook into TAP::Harness

---
+++ examples/harness-hook/lib/Harness/Hook.pm
@@ -0,0 +1,28 @@
+package Harness::Hook;
+
+use strict;
+use warnings;
+use Carp;
+
+sub new {
+    my ( $class, $harness ) = @_;
+    my $self = bless {}, $class;
+
+    $harness->callback(
+        'before_runtests',
+        sub {
+            my ($aggregate) = @_;
+            warn "Before runtests\n";
+        }
+    );
+
+    $harness->callback(
+        'after_runtests',
+        sub {
+            my ( $aggregate, $results ) = @_;
+            warn "After runtests\n";
+        }
+    );
+}
+
+1;
