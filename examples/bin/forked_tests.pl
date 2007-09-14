d9a44d3c87417a51b6f4eb34dc5932c5a464c8a7
Eric Wilhelm <scratchcomputing@gmail.com>
2007-09-14 00:33:36 +0000

    examples/bin/forked_tests.pl - update to non-fork-bombing version

--- examples/bin/forked_tests.pl
+++ examples/bin/forked_tests.pl
@@ -1,49 +1,61 @@
 #!/usr/bin/perl
-#
-# Run tests in parallel
-#
-# From: Eric Wilhelm <scratchcomputing@gmail.com>
+
+# Run tests in parallel.  This just allows you to check that your tests
+# are roughly capable of running in parallel.  It writes output to a
+# tree in /tmp.
+# From: Eric Wilhelm @ ewilhelm at cpan.org
 
 use warnings;
 use strict;
 
 use File::Basename ();
-use File::Path     ();
-use List::Util     ();
+use File::Path ();
+use List::Util ();
 
 my @tests = @ARGV;
-@tests = List::Util::shuffle(@tests);
+#@tests = List::Util::shuffle(@tests);
+
+use POSIX ();
 
 my %map;
 my $i = 0;
 
-foreach my $test (@tests) {
+my $jobs = 9; # scalar(@tests); # if you like forkbombs
+my @running;
+
+while(@tests) {
+  if($jobs == @running) {
+    my @list;
+    while(my $pid = shift(@running)) {
+      if(waitpid($pid, POSIX::WNOHANG()) > 0) {
+        warn ' ' x 25 . "done $map{$pid}\n";
+        next;
+      }
+      push(@list, $pid);
+    }
+    #warn "running ", scalar(@list);
+    @running = @list;
+    next;
+  }
+  my $test = shift(@tests);
   defined(my $pid = fork) or die;
-  $i++;
+    $i++;
   if($pid) {
+    push(@running, $pid);
     $map{$pid} = $test;
+    print "$test\n";
   }
   else {
     my $dest_base = '/tmp';
-    my $dest_dir  = File::Basename::dirname("$dest_base/$test");
+    my $dest_dir = File::Basename::dirname("$dest_base/$test");
     unless(-d $dest_dir) {
       File::Path::mkpath($dest_dir) or die;
     }
 
-    # this was an interesting hack, but not really needed
-    #$ENV{DISPLAY} = ':' . (50 + $i);
+    $| = 1;
     open(STDOUT, '>', "$dest_base/$test.out") or die;
     open(STDERR, '>', "$dest_base/$test.err") or die;
-    if(1) {
-      exec($^X, '-Ilib', $test);
-    }
-    else {
-      $0 = $test;
-      require lib;
-      lib->import('lib');
-      do($test);
-      exit;
-    }
+    exec($^X, '-Ilib', $test);
   }
 }
 
@@ -51,7 +63,7 @@ my $v = 0;
 until($v == -1) {
   $v = wait;
   ($v == -1) and last;
-  $?         and warn "$map{$v} ($v) no happy $?";
+  $? and warn "$map{$v} ($v) no happy $?";
 }
 print "bye\n";
 
