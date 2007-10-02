562a72e8f2116909f240e24788dfa2ed39bdd1c5
Andy Armstrong <andy@hexten.net>
2007-10-02 17:54:16 +0000

    Add some more settings to test

--- benchmark/parallel.sh
+++ benchmark/parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for set in pt/fast pt/fickle pt/greedy pt/gross pt/lazy pt
 do
-    for args in '' '-j9' '-j9 --fork' '-j4 --fork'
+    for args in '' '-j9' '-j4' '-j32' '-j9 --fork' '-j4 --fork' '-j32 --fork'
     do
         echo "Running prove -rQ $args $set"
         prove -rQ $args $set
