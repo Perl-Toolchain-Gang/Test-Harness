b05ad4d1d885bf7aedbca83483e1e448333d1a22
Andy Armstrong <andy@hexten.net>
2007-10-04 12:33:41 +0000

    Fix on 5.5.4

--- lib/TAP/Parser/Multiplexer.pm
+++ lib/TAP/Parser/Multiplexer.pm
@@ -75,8 +75,7 @@ the next result.
 sub add {
     my ( $self, $parser, $stash ) = @_;
 
-    my @handles = IS_WIN32 ? () : $parser->get_select_handles;
-    if (@handles) {
+    if ( IS_WIN32 && ( my @handles = $parser->get_select_handles ) ) {
         my $sel = $self->{select};
 
         # We have to turn handles into file numbers here because by
