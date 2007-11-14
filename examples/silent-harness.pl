b3f5f0d73efa5114537fbd489843d2e674457fb4
Andy Armstrong <andy@hexten.net>
2007-11-14 19:52:08 +0000

    Added comment

--- examples/silent-harness.pl
+++ examples/silent-harness.pl
@@ -10,5 +10,7 @@ use Data::Dumper;
 my @tests = glob 't/yaml*.t';
 
 my $harness = TAP::Harness->new( { verbosity => -9, lib => ['blib/lib'] } );
+
+# $aggregate is a TAP::Parser::Aggregator
 my $aggregate = $harness->runtests(@tests);
 print Dumper($aggregate);
