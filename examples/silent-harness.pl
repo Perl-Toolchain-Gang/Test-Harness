3acc86b59da568c11280555187f307f14394cb50
Andy Armstrong <andy@hexten.net>
2007-11-14 14:07:10 +0000

    Added silent-harness example

---
+++ examples/silent-harness.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#
+# Run some tests and get back a data structure describing them.
+
+use strict;
+use warnings;
+use TAP::Harness;
+use Data::Dumper;
+
+my @tests = glob 't/yaml*.t';
+
+my $harness = TAP::Harness->new( { verbosity => -9, lib => ['blib/lib'] } );
+my $aggregate = $harness->runtests(@tests);
+print Dumper($aggregate);
