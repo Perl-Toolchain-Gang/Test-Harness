1cb94d31a29bb7c03a8f133d5c7e4aa41cdc08fb
Andy Armstrong <andy@hexten.net>
2008-02-08 20:31:45 +0000

    Tagging 3.08, version bump

--- lib/TAP/Parser.pm
+++ lib/TAP/Parser.pm
@@ -19,11 +19,11 @@ TAP::Parser - Parse L<TAP|Test::Harness::TAP> output
 
 =head1 VERSION
 
-Version 3.08
+Version 3.09
 
 =cut
 
-$VERSION = '3.08';
+$VERSION = '3.09';
 
 my $DEFAULT_TAP_VERSION = 12;
 my $MAX_TAP_VERSION     = 13;
--- lib/Test/Harness.pm
+++ lib/Test/Harness.pm
@@ -41,11 +41,11 @@ Test::Harness - Run Perl standard test scripts with statistics
 
 =head1 VERSION
 
-Version 3.08
+Version 3.09
 
 =cut
 
-$VERSION = '3.08';
+$VERSION = '3.09';
 
 # Backwards compatibility for exportable variable names.
 *verbose  = *Verbose;
