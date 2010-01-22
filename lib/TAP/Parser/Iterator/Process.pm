70e0b28edced508a510c53a39ba75c86c81da051
Andy Armstrong <andy@hexten.net>
2010-01-22 11:30:58 +0000

    Hide 'undef in assignment' warning.

--- lib/TAP/Parser/Iterator/Process.pm
+++ lib/TAP/Parser/Iterator/Process.pm
@@ -79,6 +79,7 @@ Get the exit status for this iterator's process.
 
 {
 
+    local $^W;    # no warnings
     # get around a catch22 in the test suite that causes failures on Win32:
     local $SIG{__DIE__} = undef;
     eval { require POSIX; &POSIX::WEXITSTATUS(0) };
