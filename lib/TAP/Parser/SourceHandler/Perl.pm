87a53e5ce61ca0893d5fc64a8d90a924b3bb8614
Andy Armstrong <andy@hexten.net>
2010-04-11 13:01:13 +0100

    Tidied

--- lib/TAP/Parser/SourceHandler/Perl.pm
+++ lib/TAP/Parser/SourceHandler/Perl.pm
@@ -138,34 +138,34 @@ sub make_iterator {
     # TODO: does this really need to be done here?
     $class->_autoflush_stdhandles;
 
-    my ($libs, $switches) = $class->_mangle_switches(
-        $class->_filter_libs($class->_switches($source))
-    );
+    my ( $libs, $switches )
+      = $class->_mangle_switches(
+        $class->_filter_libs( $class->_switches($source) ) );
 
-    $class->_run($source, $libs, $switches);
+    $class->_run( $source, $libs, $switches );
 }
 
 sub _mangle_switches {
-    my ($class, $libs, $switches) = @_;
+    my ( $class, $libs, $switches ) = @_;
 
     # Taint mode ignores environment variables so we must retranslate
     # PERL5LIB as -I switches and place PERL5OPT on the command line
     # in order that it be seen.
-    if ( grep { $_ eq "-T" || $_ eq "-t" } @{ $switches } ) {
+    if ( grep { $_ eq "-T" || $_ eq "-t" } @{$switches} ) {
         return (
-            $libs, [
-                @{ $switches },
+            $libs,
+            [   @{$switches},
                 $class->_libs2switches($libs),
                 split_shell( $ENV{PERL5OPT} )
             ],
         );
     }
 
-    return ($libs, $switches);
+    return ( $libs, $switches );
 }
 
 sub _filter_libs {
-    my ($class, @switches) = @_;
+    my ( $class, @switches ) = @_;
 
     my $path_sep = $Config{path_sep};
     my $path_re  = qr{$path_sep};
@@ -193,12 +193,12 @@ sub _filter_libs {
 }
 
 sub _iterator_hooks {
-    my ($class, $source, $libs) = @_;
+    my ( $class, $source, $libs ) = @_;
 
     my $setup = sub {
-        if (@{ $libs }) {
-            $ENV{PERL5LIB}
-              = join( $Config{path_sep}, grep {defined} @{ $libs }, $ENV{PERL5LIB} );
+        if ( @{$libs} ) {
+            $ENV{PERL5LIB} = join( $Config{path_sep}, grep {defined} @{$libs},
+                $ENV{PERL5LIB} );
         }
     };
 
@@ -214,22 +214,22 @@ sub _iterator_hooks {
         }
     };
 
-    return ($setup, $teardown);
+    return ( $setup, $teardown );
 }
 
 sub _run {
-    my ($class, $source, $libs, $switches) = @_;
+    my ( $class, $source, $libs, $switches ) = @_;
 
     my @command = $class->_get_command_for_switches( $source, $switches )
       or $class->_croak("No command found!");
 
-    my ($setup, $teardown) = $class->_iterator_hooks($source, $libs);
+    my ( $setup, $teardown ) = $class->_iterator_hooks( $source, $libs );
 
-    return $class->_create_iterator($source, \@command, $setup, $teardown);
+    return $class->_create_iterator( $source, \@command, $setup, $teardown );
 }
 
 sub _create_iterator {
-    my ($class, $source, $command, $setup, $teardown) = @_;
+    my ( $class, $source, $command, $setup, $teardown ) = @_;
 
     return TAP::Parser::Iterator::Process->new(
         {   command  => $command,
@@ -248,7 +248,7 @@ sub _get_command_for_switches {
 
    # XXX don't need to quote if we treat the parts as atoms (except maybe vms)
    #$file = qq["$file"] if ( $file =~ /\s/ ) && ( $file !~ /^".*"$/ );
-    my @command = ( $command, @{ $switches }, $file, @args );
+    my @command = ( $command, @{$switches}, $file, @args );
     return @command;
 }
 
