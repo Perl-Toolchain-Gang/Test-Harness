cae7f2fe784ab1906aaaee0f0942d73df9f32ba2
Leon Timmermans <fawaka@gmail.com>
2014-08-16 14:53:08 +0200

    Bump version to 3.33

--- lib/TAP/Formatter/File.pm
+++ lib/TAP/Formatter/File.pm
@@ -13,11 +13,11 @@ TAP::Formatter::File - Harness output delegate for file output
 
 =head1 VERSION
 
-Version 3.32
+Version 3.33
 
 =cut
 
-our $VERSION = '3.32';
+our $VERSION = '3.33';
 
 =head1 DESCRIPTION
 
