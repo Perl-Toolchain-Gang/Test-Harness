c6381af54de9e034a6feed1995f656f6ba4a9f11
Leon Timmermans <fawaka@gmail.com>
2015-12-30 13:01:18 +0100

    Bump version to 3.36

--- lib/TAP/Parser/YAMLish/Reader.pm
+++ lib/TAP/Parser/YAMLish/Reader.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base 'TAP::Object';
 
-our $VERSION = '3.35';
+our $VERSION = '3.36';
 
 # TODO:
 #   Handle blessed object syntax
@@ -269,7 +269,7 @@ TAP::Parser::YAMLish::Reader - Read YAMLish data from iterator
 
 =head1 VERSION
 
-Version 3.35
+Version 3.36
 
 =head1 SYNOPSIS
 
