bbdd07f8d5202f0ee52abbeb6c19790c9a895638
Reini Urban <rurban@cpan.org>
2018-09-06 10:00:27 +0200

    3.42_01 as in cperl release 5.28.0c

--- lib/TAP/Parser/Iterator/Process.pm
+++ lib/TAP/Parser/Iterator/Process.pm
@@ -16,11 +16,11 @@ TAP::Parser::Iterator::Process - Iterator for process-based TAP sources
 
 =head1 VERSION
 
-Version 3.42
+Version 3.42_01
 
 =cut
 
-our $VERSION = '3.42';
+our $VERSION = '3.42_01';
 
 =head1 SYNOPSIS
 
--- lib/TAP/Parser/Result/Unknown.pm
+++ lib/TAP/Parser/Result/Unknown.pm
@@ -11,11 +11,11 @@ TAP::Parser::Result::Unknown - Unknown result token.
 
 =head1 VERSION
 
-Version 3.42
+Version 3.42_01
 
 =cut
 
-our $VERSION = '3.42';
+our $VERSION = '3.42_01';
 
 =head1 DESCRIPTION
 
