6eacd6cbcfb61cc476197fee90196f445458d2d4
Leon Timmermans <fawaka@gmail.com>
2023-08-13 11:51:06 +0200

    Add missing negation in EINTR check

--- lib/TAP/Parser/Multiplexer.pm
+++ lib/TAP/Parser/Multiplexer.pm
@@ -134,7 +134,7 @@ sub _iter {
         until (@ready) {
             return unless $sel->count;
             @ready = $sel->can_read;
-            last if @ready || $!{EINTR};
+            last if @ready || !$!{EINTR};
         }
 
         my ( $h, $parser, $stash, @handles ) = @{ shift @ready };
